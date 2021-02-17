870570f1eddfaf1d7609631b0490255f5159d6a8
Hideaki Kawai <kayhide@gmail.com>
2021-02-18 01:52:22 +0900

    Create user list

--- frontend/src/App.svelte
+++ frontend/src/App.svelte
@@ -1,36 +1,69 @@
 <script>
-	import TailwindStyles from './TailwindStyles.svelte';
-	
-	const message = 'Learn Svelte';
+    import axios from 'axios';
+    import TailwindStyles from './TailwindStyles.svelte';
+    import { onMount } from 'svelte';
+    
+    const message = 'Learn Svelte';
+
+    let name = "";
+    let email = "";
+    let users = [];
+    function loadUsers() {
+        axios
+            .get("http://localhost:3000/api/users")
+            .then(res => { users = res.data; });
+    }
+
+    function handleCreateUser(event) {
+        event.preventDefault();
+        event.stopPropagation();
+        axios
+            .post("http://localhost:3000/api/users", { name, email })
+            .then(res => {
+                console.log(res.data);
+                loadUsers();
+
+                })
+
+
+    }
+
+    onMount(() => {
+        loadUsers();
+    });
 </script>
 
 <style>
-	.App-logo {
-		animation: App-logo-scale infinite 1.6s ease-in-out alternate;
-	}
-	@keyframes App-logo-scale {
-		from {
-			transform: scale(1);
-		}
-		to {
-			transform: scale(1.06);
-		}
-	}
+    .control {
+        @apply px-4 py-2 border rounded
+    }
+    .gray {
+        @apply text-gray-900 border-gray-400 hover:bg-gray-100
+    }
+    .blue {
+        @apply text-blue-900 border-blue-400 hover:bg-blue-100
+    }
 </style>
 
-<div class="text-center font-serif">
-	<header class="bg-gray-100 h-screen flex justify-center items-center flex-col text-3xl">
-		<img src="/logo.svg" class="App-logo pointer-events-none m-4 h-64" alt="logo" />
-		<p>
-			Edit <code>src/App.svelte</code> and save to reload.
-		</p>
-		<a
-			class="text-orange-500"
-			href="https://svelte.dev"
-			target="_blank"
-			rel="noopener noreferrer"
-		>
-			{message}
-		</a>
-	</header>
+<div class="m-16 font-sanserif">
+    <div class="text-lg">Users</div>
+    <form class="my-4" on:submit={handleCreateUser}>
+        <input class="control gray" bind:value={name} placeholder="Name">
+        <input class="control gray" bind:value={email} placeholder="Email">
+        <button
+            class="control blue"
+            type="submit"
+        >
+            Create
+        </button>
+    </form>
+    <div class="text-right">Count: {users.length}</div>
+    <ul class="my-2 py-2 bg-gray-100 rounded-lg">
+        {#each users as user}
+        <li class="w-h flex px-4 py-2 border-b border-gray-400">
+            <div class="w-1/2">{user.name}</div>
+            <div class="w-1/2">{user.email}</div>
+        </li>
+        {/each}
+    </ul>
 </div>
