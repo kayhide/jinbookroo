1e3609e7408da06aa5a9ac8a1f45602ad8025a4a
Hideaki Kawai <kayhide@gmail.com>
2021-02-25 00:37:47 +0900

    Add password_hash to users

--- phoenix/lib/jinbookroo/accounts/user.ex
+++ phoenix/lib/jinbookroo/accounts/user.ex
@@ -5,6 +5,8 @@ defmodule Jinbookroo.Accounts.User do
   schema "users" do
     field :email, :string
     field :name, :string
+    field :password_hash, :string
+    field :password, :string, virtual: true
 
     timestamps()
   end
@@ -12,8 +14,17 @@ defmodule Jinbookroo.Accounts.User do
   @doc false
   def changeset(user, attrs) do
     user
-    |> cast(attrs, [:name, :email])
+    |> cast(attrs, [:name, :email, :password])
     |> validate_required([:name, :email])
     |> unique_constraint(:email)
+    |> put_password_hash()
   end
+
+
+  defp put_password_hash(%Ecto.Changeset{valid?: true, changes: %{password: password}} = changeset) do
+    changeset
+    |> change(%{password_hash: Argon2.hash_pwd_salt(password), password: nil})
+  end
+
+  defp put_password_hash(changeset), do: changeset
 end
--- phoenix/mix.exs
+++ phoenix/mix.exs
@@ -42,6 +42,8 @@ defmodule Jinbookroo.MixProject do
       {:telemetry_poller, "~> 0.4"},
       {:gettext, "~> 0.11"},
       {:jason, "~> 1.0"},
+      {:comeonin, "~> 5.0"},
+      {:argon2_elixir, "~> 2.0"},
       {:cors_plug, "~> 2.0"},
       {:plug_cowboy, "~> 2.0"}
     ]
