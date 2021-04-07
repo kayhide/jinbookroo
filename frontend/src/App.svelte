ae51b9468937d5e9234152a69ea05d128212da18
Hideaki Kawai <kayhide@gmail.com>
2021-04-08 00:15:55 +0900

    Add router

--- frontend/src/App.svelte
+++ frontend/src/App.svelte
@@ -1,49 +1,28 @@
 <script>
+  import { Route, Router, Link } from "svelte-routing";
   import TailwindStyles from "./TailwindStyles.svelte";
-  import { onMount } from "svelte";
-  import * as Users from "./Api/Users.js";
+  import UserList from "./App/UserList.svelte";
 
-  let name = "";
-  let email = "";
-  const users = Users.items();
+  export let url = "/";
 
-  function handleCreateUser(event) {
-    event.preventDefault();
-    event.stopPropagation();
-    users.create({ name, email });
-  }
-
-  onMount(() => {
-    users.list();
+  history.pushState = new Proxy(history.pushState, {
+    apply(target, arg, args) {
+      url = args[2];
+      Reflect.apply(target, arg, args);
+    },
   });
 </script>
 
-<style>
-  .control {
-    @apply px-4 py-2 border rounded;
-  }
-  .gray {
-    @apply text-gray-900 border-gray-400 hover:bg-gray-100;
-  }
-  .blue {
-    @apply text-blue-900 border-blue-400 hover:bg-blue-100;
-  }
-</style>
-
-<div class="m-16 font-sanserif">
-  <div class="text-lg">Users</div>
-  <form class="my-4" on:submit="{handleCreateUser}">
-    <input class="control gray" bind:value="{name}" placeholder="Name" />
-    <input class="control gray" bind:value="{email}" placeholder="Email" />
-    <button class="control blue" type="submit"> Create </button>
-  </form>
-  <div class="text-right">Count: {$users.length}</div>
-  <ul class="my-2 py-2 bg-gray-100 rounded-lg">
-    {#each $users as user}
-      <li class="w-h flex px-4 py-2 border-b border-gray-400">
-        <div class="w-1/2">{user.name}</div>
-        <div class="w-1/2">{user.email}</div>
-      </li>
-    {/each}
-  </ul>
+<div class="m-2">
+  {url}
 </div>
+<Router url="{url}">
+  <nav>
+    <Link to="/">Users</Link>
+    <Link to="/users">Users</Link>
+  </nav>
+  <div>
+    <Route path="/"><UserList /></Route>
+    <Route path="/users"><UserList /></Route>
+  </div>
+</Router>
---
+++ frontend/src/App/UserList.svelte
@@ -0,0 +1,48 @@
+<script>
+  import { onMount } from "svelte";
+  import * as Users from "../Api/Users.js";
+
+  let name = "";
+  let email = "";
+  const users = Users.items();
+
+  function handleCreateUser(event) {
+    event.preventDefault();
+    event.stopPropagation();
+    users.create({ name, email });
+  }
+
+  onMount(() => {
+    users.list();
+  });
+</script>
+
+<style>
+  .control {
+    @apply px-4 py-2 border rounded;
+  }
+  .gray {
+    @apply text-gray-900 border-gray-400 hover:bg-gray-100;
+  }
+  .blue {
+    @apply text-blue-900 border-blue-400 hover:bg-blue-100;
+  }
+</style>
+
+<div class="m-16 font-sanserif">
+  <div class="text-lg">Users</div>
+  <form class="my-4" on:submit="{handleCreateUser}">
+    <input class="control gray" bind:value="{name}" placeholder="Name" />
+    <input class="control gray" bind:value="{email}" placeholder="Email" />
+    <button class="control blue" type="submit"> Create </button>
+  </form>
+  <div class="text-right">Count: {$users.length}</div>
+  <ul class="my-2 py-2 bg-gray-100 rounded-lg">
+    {#each $users as user}
+      <li class="w-h flex px-4 py-2 border-b border-gray-400">
+        <div class="w-1/2">{user.name}</div>
+        <div class="w-1/2">{user.email}</div>
+      </li>
+    {/each}
+  </ul>
+</div>
